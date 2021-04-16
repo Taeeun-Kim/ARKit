6854bc59dd6ba4039d7d9d30d893cfe1a2ef2d8c
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-16 11:33:55 +0200

    Initial commit

---
+++ ARKitGame/ARKitGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARKitGame
+//
+//  Created by Taeeun Kim on 16.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
