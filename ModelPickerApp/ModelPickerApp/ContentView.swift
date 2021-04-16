d7cae5b34d4729578d0fc275b5e72c0d94905aec
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-16 15:38:08 +0200

    Add image files and buttons

--- ModelPickerApp/ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ModelPickerApp/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    
+    var models: [String] = ["toy_biplane", "tv_retro", "gramophone", "teapot"]
+    
     var body: some View {
-        Text("Hello World")
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< self.models.count) { index in
+                        Button(action: {
+                            print("DEBUG: selected model with name: \(self.models[index])")
+                        }, label: {
+                            Image(uiImage: UIImage(named: self.models[index])!)
+                                .resizable()
+                                .frame(height: 80)
+                                .aspectRatio(1/1, contentMode: .fit)
+                                .background(Color.white)
+                                .cornerRadius(12)
+                        })
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            } //: SCROLLVIEW
+            .padding(20)
+            .background(Color.black.opacity(0.5))
+        }
     }
 }
 
