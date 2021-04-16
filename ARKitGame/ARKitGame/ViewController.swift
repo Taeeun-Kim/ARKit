3afb7d14824d76ec9f6a9d66bb76b68fe87e7588
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-16 12:17:14 +0200

    Update Storyboard and ViewController for AR

--- ARKitGame/ARKitGame/ViewController.swift
+++ ARKitGame/ARKitGame/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let scene = SCNScene()
+        
+        sceneView.scene = scene
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
     }
 
 
