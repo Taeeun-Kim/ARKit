169d20ecba8b518e81a6081b94d4b2845832db60
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-16 16:56:39 +0200

    Update code

--- ModelPickerApp/ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ModelPickerApp/ContentView.swift
@@ -9,33 +9,38 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
     
-    var models: [String] = ["toy_biplane", "tv_retro", "gramophone", "teapot"]
+    private var models: [String] = ["toy_biplane", "tv_retro", "gramophone", "teapot"]
+//    private var models: [String] = {
+//        // Dynamically get our model filenames
+//        let filemanager = FileManager.default
+//
+//        // guaard, because these are optional values
+//        guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {
+//                return []
+//        } // error occurred
+//
+//        var availableModels: [String] = []
+//        for filename in files where filename.hasSuffix("usdz") {
+//            let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+//            availableModels.append(modelName)
+//        }
+//
+//        return availableModels
+//    }()
     
     var body: some View {
         ZStack(alignment: .bottom) {
             ARViewContainer()
             
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 30) {
-                    ForEach(0 ..< self.models.count) { index in
-                        Button(action: {
-                            print("DEBUG: selected model with name: \(self.models[index])")
-                        }, label: {
-                            Image(uiImage: UIImage(named: self.models[index])!)
-                                .resizable()
-                                .frame(height: 80)
-                                .aspectRatio(1/1, contentMode: .fit)
-                                .background(Color.white)
-                                .cornerRadius(12)
-                        })
-                        .buttonStyle(PlainButtonStyle())
-                    }
-                }
-            } //: SCROLLVIEW
-            .padding(20)
-            .background(Color.black.opacity(0.5))
+            if self.isPlacementEnabled {
+                PlacementButtonsView()
+            } else {
+                ModelPickerView(models: self.models)
+            }
         }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -51,6 +56,63 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< self.models.count) { index in
+                    Button(action: {
+                        print("DEBUG: selected model with name: \(self.models[index])")
+                    }, label: {
+                        Image(uiImage: UIImage(named: self.models[index])!)
+                            .resizable()
+                            .frame(height: 60)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.white)
+                            .cornerRadius(12)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        } //: SCROLLVIEW
+        .padding(20)
+        .background(Color.black.opacity(0.5))
+
+    }
+}
+
+struct PlacementButtonsView: View {
+    var body: some View {
+        HStack{
+            // Cancel Button
+            Button(action: {
+                print("DEBUG: Cancel model placement.")
+            }, label: {
+                Image(systemName: "xmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            })
+            
+            // Confirm Button
+            Button(action: {
+                print("DEBUG: model placement comfirmed.")
+            }, label: {
+                Image(systemName: "checkmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            })
+        }
+    }
 }
 
 #if DEBUG
