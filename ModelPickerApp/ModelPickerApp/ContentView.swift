b48b5b89862e2527587e3851b4805fc721e44b34
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-16 20:13:31 +0200

    save

--- ModelPickerApp/ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ModelPickerApp/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+//import FocusEntity
 
 struct ContentView : View {
     @State private var isPlacementEnabled = false
@@ -54,18 +55,8 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
-        
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal, .vertical]
-        config.environmentTexturing = .automatic
-        
-        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
-            config.sceneReconstruction = .mesh
-        }
-        
-        arView.session.run(config)
-        
+        let arView = CustomARView(frame: .zero)
+                
         return arView
         
     }
@@ -91,6 +82,47 @@ struct ARViewContainer: UIViewRepresentable {
         }
     }
     
+}
+
+class CustomARView: ARView {
+//    let focusSquare = FocusEntity()
+    let focusSquare = FocusEntity(on: self.arView, style: .classic)
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        
+//        focusSquare.viewDelegate = self
+        focusSquare.delegate = self
+        focusSquare.setAutoUpdate(to: true)
+        
+        self.setupARView()
+    }
+    
+    @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupARView() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        self.session.run(config)
+    }
+}
+
+extension CustomARView: FocusEntityDelegate {
+    func toTrackingState() {
+        print("tracking")
+    }
+    
+    func toInitializingState() {
+        print("initializing")
+    }
 }
 
 struct ModelPickerView: View {
