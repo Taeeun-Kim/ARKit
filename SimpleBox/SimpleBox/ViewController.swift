edde1f374632201b89366d0eced3ab7b71e89ce8
Taeeun Kim <taeeun.kim@outlook.com>
2021-05-25 22:03:03 +0200

    SimpleBox in ARKit

---
+++ SimpleBox/SimpleBox/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  SimpleBox
+//
+//  Created by Taeeun Kim on 25.05.21.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        let box = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0) // not cm, it's meter
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        box.materials = [material]
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(0, 0, -0.5) // 3차원, 나로부터 0.5미터 떨어지게끔
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
