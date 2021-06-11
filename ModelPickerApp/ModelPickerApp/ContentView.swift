704328b4dcdfc1e430a5d20e8bd15ce518190d7a
Taeeun Kim <taeeun.kim@outlook.com>
2021-06-11 14:32:09 +0200

    Delete FocusEntity Package and add Becherglas.usdz

--- ModelPickerApp/ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ModelPickerApp/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
-import FocusEntity
+//import FocusEntity
 
 struct ContentView : View {
     @State private var isPlacementEnabled = false
@@ -55,7 +55,17 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = CustomARView(frame: .zero)
+        let arView = ARView(frame: .zero)
+        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        arView.session.run(config)
                 
         return arView
         
@@ -84,45 +94,45 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
-class CustomARView: ARView {
-    let focusSquare = FESquare()
-    
-    required init(frame frameRect: CGRect) {
-        super.init(frame: frameRect)
-        
-        focusSquare.viewDelegate = self
-        focusSquare.delegate = self
-        focusSquare.setAutoUpdate(to: true)
-        
-        self.setupARView()
-    }
-    
-    @objc required dynamic init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func setupARView() {
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal, .vertical]
-        config.environmentTexturing = .automatic
-        
-        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
-            config.sceneReconstruction = .mesh
-        }
-        
-        self.session.run(config)
-    }
-}
-
-extension CustomARView: FEDelegate {
-    func toTrackingState() {
-        print("tracking")
-    }
-    
-    func toInitializingState() {
-        print("initializing")
-    }
-}
+//class CustomARView: ARView {
+//    let focusSquare = FESquare()
+//
+//    required init(frame frameRect: CGRect) {
+//        super.init(frame: frameRect)
+//
+//        focusSquare.viewDelegate = self
+//        focusSquare.delegate = self
+//        focusSquare.setAutoUpdate(to: true)
+//
+//        self.setupARView()
+//    }
+//
+//    @objc required dynamic init?(coder decoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    func setupARView() {
+//        let config = ARWorldTrackingConfiguration()
+//        config.planeDetection = [.horizontal, .vertical]
+//        config.environmentTexturing = .automatic
+//
+//        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+//            config.sceneReconstruction = .mesh
+//        }
+//
+//        self.session.run(config)
+//    }
+//}
+//
+//extension CustomARView: FEDelegate {
+//    func toTrackingState() {
+//        print("tracking")
+//    }
+//
+//    func toInitializingState() {
+//        print("initializing")
+//    }
+//}
 
 struct ModelPickerView: View {
     @Binding var isPlacementEnabled: Bool
