c049f7ede2c82fbd0a0ea773f339f3f8824460bd
Taeeun Kim <taeeun.kim@outlook.com>
2021-08-24 16:27:45 +0200

    Revert "Revert "FocusEntitiy Version""
    This reverts commit 8ff1f4976d32136d1ea54d8504c3254073dff0cd.

--- RealityKit-Tutorial/RealityKit-Tutorial/ContentView.swift
+++ RealityKit-Tutorial/RealityKit-Tutorial/ContentView.swift
@@ -28,17 +28,57 @@ struct ContentView: View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    @EnvironmentObject var placementSettings: PlacementSettings
     
-    func makeUIView(context: Context) -> ARView {
+    func makeUIView(context: Context) -> CustomARView {
         
-        let arView = ARView(frame: .zero)
+        let arView = CustomARView(frame: .zero)
+        
+        // Subscribe to SceneEvents.Update
+        self.placementSettings.sceneObserver = arView.scene.subscribe(to: SceneEvents.Update.self, { event in
+            
+            self.updateScene(for: arView )
+            
+        })
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: CustomARView, context: Context) {}
     
+    private func updateScene(for arView: CustomARView) {
+        
+        // Only display focusEntity when the user has selected a model for placement
+        arView.focusEntity?.isEnabled = self.placementSettings.selectedModel != nil
+        
+        // Add model to scene if confirmed for placement
+        if let confirmedModel = self.placementSettings.confirmedModel, let modelEntity = confirmedModel.modelEntity {
+            
+            self.place(modelEntity, in: arView)
+            
+            self.placementSettings.confirmedModel = nil
+        }
+    }
+    
+    private func place(_ modelEntity: ModelEntity, in arView: ARView) {
+        
+        // 1. Clone modelEntity. This creates an identical copy of modelEntity and references the same model. This also allows us to have multiple models of the same asset in our scene.
+        let clonedEntity = modelEntity.clone(recursive: true)
+        
+        // 2. Enable translation and rotation gestures.
+        clonedEntity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.translation, .rotation], for: clonedEntity)
+        
+        // 3. Create an anchorEntity and add clonedEntity to the anchorEntity.
+        let anchorEntity = AnchorEntity(plane: .any)
+        anchorEntity.addChild(clonedEntity)
+        
+        // 4. Add the anchorEntity to the arView.scene
+        arView.scene.addAnchor(anchorEntity)
+        
+        print("Added modelEntity to scene.")
+    }
 }
 
 #if DEBUG
---
+++ RealityKit-Tutorial/RealityKit-Tutorial/CustomARView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomARView.swift
+//  RealityKit-Tutorial
+//
+//  Created by Taeeun Kim on 24.08.21.
+//
+
+import RealityKit
+import ARKit
+import FocusEntity
+
+class CustomARView: ARView {
+    
+    var focusEntity: FocusEntity?
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        
+        focusEntity = FocusEntity(on: self, focus: .classic)
+        
+    }
+    
+    @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        session.run(config)
+    }
+}
