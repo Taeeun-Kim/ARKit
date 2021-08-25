4564f68f8ffb8afea24977ef12bdcc7182891d8e
Taeeun Kim <taeeun.kim@outlook.com>
2021-08-25 21:48:00 +0200

    test .mesh

--- MeshLiDAR/MeshLiDAR/ContentView.swift
+++ MeshLiDAR/MeshLiDAR/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -20,11 +21,18 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+        
+        let config = ARWorldTrackingConfiguration()
+        config.sceneReconstruction = .mesh
+
+        arView.debugOptions.insert([.showSceneUnderstanding])
+        arView.environment.sceneUnderstanding.options.insert([.physics])
+        arView.session.run(config)
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
